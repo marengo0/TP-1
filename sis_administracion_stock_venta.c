3ab59367ce8554f7906714d7eb874b43e32f585b
Franco Loizzo <loizzoenzo.97@gmail.com>
2023-05-25 19:59:53 -0300

    Pongo func dia en case 3, agrego codigo de actualizar stock (ver comentarios)

--- sis_administracion_stock_venta.c
+++ sis_administracion_stock_venta.c
@@ -16,8 +16,16 @@ Alumnos: Lucas Galeano, Franco Puzzio, Leandro Chanarian
 
 int fun_ventas (int);
 void func_mostrar_dia(int);
+
 int main() {
-	int opc; int quesoStock = 20; int unidad; int gananciaDia = 0; int gananciaSemanal = 0; int n = 1;
+	int opc;
+	int quesoStock = 0; 
+	int unidad; 
+	int gananciaDia = 0; 
+	int gananciaSemanal = 0; 
+	int n = 1;
+	
+	
 	printf("Sistema de control de stock/venta\n");
 	printf("=================================\n");
     printf(" \n");
@@ -35,87 +43,92 @@ int main() {
 	
 	while(opc != 0){
 	
-	switch(opc){
-		case 1:
+		switch(opc){
+			case 1:
 			
-			//La funcion fun_ventas se ingresa en case 1, elimina todo lo que hay en case 1
-			gananciaDia = fun_ventas(gananciaDia);
-			gananciaSemanal = gananciaSemanal + gananciaDia;
+				//La funcion fun_ventas se ingresa en case 1, elimina todo lo que hay en case 1
+				gananciaDia = fun_ventas(gananciaDia);
+				gananciaSemanal = gananciaSemanal + gananciaDia;
 			
-			break;
+				break;
 			
-		case 2:
-		/* actualizar el stock de cada producto, TAREA PENDIENTE */
-			system("cls");
-			printf("LISTA DE PRODUCTOS\n");
-			printf("==================");
-			printf("\nid|nombre producto|stock|precio\n");
-			printf("\n001|Queso cremoso  |%d|250$\n",quesoStock);
-			printf("\n002|Queso muzarella|%d|250$\n",quesoStock);
-			printf("\n003|Queso roquefort|%d|250$\n",quesoStock);
-			printf("\n004|Queso cheddar  |%d|250$\n",quesoStock);
-			printf("\n005|Queso sardo    |%d|250$\n",quesoStock);
-			printf("\n006|Queso gruyere  |%d|250$\n",quesoStock);
-			printf("\n007|Queso parmesano|%d|250$\n",quesoStock);
-			printf("\n008|Queso azul     |%d|250$\n",quesoStock);
-			system("pause");
-			break;
+			case 2:
+				if(quesoStock == 0){
+					printf("Lo sentimos, no tenemos mas stock por hoy :c\n");
+				}else if(quesoStock > 0){
+								/* actualizar el stock de cada producto, TAREA PENDIENTE */
+					system("cls");
+					printf("LISTA DE PRODUCTOS\n");
+					printf("==================");
+					printf("\nid|nombre producto|stock|precio\n");
+					printf("\n001|Queso cremoso  |%d|250$\n",quesoStock);
+					printf("\n002|Queso muzarella|%d|250$\n",quesoStock);
+					printf("\n003|Queso roquefort|%d|250$\n",quesoStock);
+					printf("\n004|Queso cheddar  |%d|250$\n",quesoStock);
+					printf("\n005|Queso sardo    |%d|250$\n",quesoStock);
+					printf("\n006|Queso gruyere  |%d|250$\n",quesoStock);
+					printf("\n007|Queso parmesano|%d|250$\n",quesoStock);
+					printf("\n008|Queso azul     |%d|250$\n",quesoStock);
+					system("pause");
+					break;
+				}
+
 		
-		case 3:
+			case 3:
 			/*la variable "ganancia" no sumaba "unidad" sino que se le asignaba el resultado de unidad*precio del producto
 			Se corrigio igualando a "ganancia" por su valor sumado por el producto de (unidad*x)
 
 			NOTA: no podemos calcular la ganancia en "case 3" ya que siempre multiplicara a unidad por 250
 			pienso que habra que o generarla dentro de una funcion o meterla en "case 2" -Lucas*/ 
 		    
-		system("cls");
-			
-
-			
-			if(n<7){
-				printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-				printf("el ingreso total fue de: $ %d\n",gananciaDia);
-				printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-			}else if (n=7){
-				printf("el ingreso total fue de: $ %d\n",gananciaDia);
-				printf("Las ganancias de la semana fueron de $ %d\n",gananciaSemanal);
-			}
-			system("pause");
-            break;
+				system("cls");
+				func_mostrar_dia(n);
+				if(n<7){
+					printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+					printf("el ingreso total fue de: $ %d\n",gananciaDia);
+					printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+				}else if (n=7){
+					printf("el ingreso total fue de: $ %d\n",gananciaDia);
+					printf("Las ganancias de la semana fueron de $ %d\n",gananciaSemanal);
+				}
+				system("pause");
+        	    break;
         
-		case 4:
-		    n++;
-		     //edite la linea 73 de "if(n<7) por if(n<=7) en caso de realizar una compra en el dia 7 y que genere errores, ya que si n = 7 --> n NO es menor a 7" -Lucas
-            if(n<=7){
-			/* gananciaSemanal = gananciaSemanal + gananciaDia; */
-			gananciaDia = 0;
-			//ganancia = 0 el nuevo dia se arranca con $ 0 ganancias
-			}else if (n==8) { //este else discrimina el fin de la semana -Franco
-				gananciaSemanal = 0;
+			case 4:
+			    n++;
+			     //edite la linea 73 de "if(n<7) por if(n<=7) en caso de realizar una compra en el dia 7 y que genere errores, ya que si n = 7 --> n NO es menor a 7" -Lucas
+   		        if(n<=7){
+				/* gananciaSemanal = gananciaSemanal + gananciaDia; */
 				gananciaDia = 0;
-				n = 1;
-			}
-		
+				//ganancia = 0 el nuevo dia se arranca con $ 0 ganancias
+				}else if (n==8) { //este else discrimina el fin de la semana -Franco
+					gananciaSemanal = 0;
+					gananciaDia = 0;
+					n = 1;
+				}
+				
+				if(quesoStock < 20){
+					quesoStock = 20;
+				}
+		}
+
+        system("cls");
+		printf("Sistema de control de stock/venta\n");
+		printf("=================================\n");
+        printf(" \n");
+ 	    func_mostrar_dia(n);
+		printf("MENU PRINCIPAL DE CONTROL\n");
+		printf("-------------------------\n");
+		printf("1.Registrar venta/s\n");
+		printf("2.Ver stock disponible\n");
+		printf("3.Generar reporte de ingreso\n");
+		printf("4.Cerrar el negocio por hoy\n");
+		printf("0.salir\n");
+		printf("Introduzca opcion:");
+		scanf("%d",&opc);	
 
 	}
-
-    system("cls");
-	printf("Sistema de control de stock/venta\n");
-	printf("=================================\n");
-    printf(" \n");
-    func_mostrar_dia(n);
-	printf("MENU PRINCIPAL DE CONTROL\n");
-	printf("-------------------------\n");
-	printf("1.Registrar venta/s\n");
-	printf("2.Ver stock disponible\n");
-	printf("3.Generar reporte de ingreso\n");
-	printf("4.Cerrar el negocio por hoy\n");
-	printf("0.salir\n");
-	printf("Introduzca opcion:");
-	scanf("%d",&opc);	
-
-}
-return 0;
+	return 0;
 
 }
 
